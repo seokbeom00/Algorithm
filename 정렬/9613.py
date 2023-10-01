520e8c29c1fbd5b68d2992a36cc4bd87d57bbf48
HongGit <soojam@konkuk.ac.kr>
2023-10-01 11:09:09 +0900

    9613 solve

---
+++ 정렬/9613.py
@@ -0,0 +1,15 @@
+import sys
+import math
+
+input = sys.stdin.readline
+
+n = int(input())
+for i in range(n):
+    arr = list(map(int, input().split()))
+    ans = 0
+    for j in range(1, len(arr)):
+        if j == len(arr) - 1:
+            break
+        for k in range(j + 1, len(arr)):
+            ans += math.gcd(arr[j], arr[k])
+    print(ans)
