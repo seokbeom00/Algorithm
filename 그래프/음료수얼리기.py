0a58df7d594f41a180ab11d2e01b581c3618f60e
HongGit <soojam@konkuk.ac.kr>
2023-11-11 16:52:35 +0900

    dfs 기본 예제 solve

--- 그래프/음료수얼리기.py
+++ 그래프/음료수얼리기.py
@@ -1,4 +1,25 @@
-import sys
+n, m = map(int, input().split())
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input())))
 
-input = sys.stdin.readline
-n, m = map(int, input())
+
+def dfs(x, y):
+    if x <= -1 or x >= n or y <= -1 or y >= m:
+        return False
+    if graph[x][y] == 0:
+        graph[x][y] = 1
+        dfs(x - 1, y)
+        dfs(x + 1, y)
+        dfs(x, y - 1)
+        dfs(x, y + 1)
+        return True
+    return False
+
+
+ans = 0
+for i in range(n):
+    for j in range(m):
+        if dfs(i, j) == True:
+            ans += 1
+print(ans)
