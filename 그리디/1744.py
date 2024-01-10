4cae8e98d87c18d1e84170fb3c8f34839ec181bf
HongGit <sukbum1020@gmail.com>
2024-01-11 01:42:58 +0900

    2089 solve

--- 그리디/1744.py
+++ 그리디/1744.py
@@ -6,36 +6,31 @@ arr = []
 for i in range(n):
     arr.append(int(input()))
 arr = sorted(arr, reverse=True)
-ans = 0
-negative_part = sorted(filter(lambda x: x < 0, arr))
-non_negative_index = next((i for i, x in enumerate(arr) if x < 0), len(arr))
-arr = arr[:non_negative_index] + negative_part
-zero_count = len([x for x in arr if x == 0])  # 0의 개수
-nc = len([x for x in arr if x < 0])  # 음수의 개수
-for i in range(zero_count):
-    arr.remove(0)
-    if nc % 2 == 1:
-        if arr[len(arr) - 1] < 0:
-            arr.pop()
-while True:
-    if 1 not in arr:
-        break
-    arr.remove(1)
-    ans += 1
+one = 0
 
-n = len(arr)
-print(arr)
-for i in range(0, n, 2):
-    if i == n - 1:
-        ans += arr[i]
+
+def calcul(arr, n):
+    ans = 0
+    for i in range(0, n, 2):
+        if i == n - 1:
+            ans += arr[i]
+            break
+        a, b = arr[i], arr[i + 1]
+        ans += a * b
+    return ans
+
+
+positive = sorted([x for x in arr if x > 0], reverse=True)
+negative = sorted([x for x in arr if x < 0])
+while True:
+    if 0 not in arr:
         break
-    a, b = arr[i], arr[i + 1]
-    if a == 1 or b == 1:
-        ans += a + b
-    elif a > 0 and b > 0:
-        ans += a * b
-    elif a < 0 and b < 0:
-        ans += a * b
-    elif (a > 0 and b < 0) or (b > 0 and a < 0):
-        ans += a + b
-print(ans)
+    arr.remove(0)
+    if len(negative) % 2 == 1:
+        negative.pop()
+while True:
+    if 1 not in positive:
+        break
+    positive.remove(1)
+    one += 1
+print(calcul(positive, len(positive)) + calcul(negative, len(negative)) + one)
