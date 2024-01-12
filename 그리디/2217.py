ac2c06ab477ef785543f4f55c1196c4a0ab86a7f
HongGit <soojam@konkuk.ac.kr>
2024-01-12 11:44:11 +0900

    2217 solved

---
+++ 그리디/2217.py
@@ -0,0 +1,15 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+n = int(input())
+w = []
+for i in range(n):
+    w.append(int(input()))
+w = sorted(w, reverse=True)
+ans = []
+cnt = 1
+for i in w:
+    ans.append(i * cnt)
+    cnt += 1
+print(max(an))
