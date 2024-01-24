40c6b16bd09f05dba4662978084d150ad4e38d02
HongGit <soojam@konkuk.ac.kr>
2024-01-24 10:16:04 +0900

    14503 clear

--- 구현/14503.py
+++ 구현/14503.py
@@ -30,37 +30,29 @@ def move_rvc(x, y, d):
         x += 1
     if d == 3 and y - 1 >= 0 and maps[x][y - 1] == 0:  # 서
         y -= 1
+    return x, y
 
 
 def back_rvc(x, y, d):
     if d == 2 and x - 1 >= 0 and maps[x - 1][y] != 1:  # 남 후진
         x -= 1
-        return True
+        return x, y
     if d == 3 and y + 1 <= m - 1 and maps[x][y + 1] != 1:  # 서 후진
         y += 1
-        return True
+        return x, y
     if d == 0 and x + 1 <= n - 1 and maps[x + 1][y] != 1:  # 북 후진
         x += 1
-        return True
+        return x, y
     if d == 1 and y - 1 >= 0 and maps[x][y - 1] != 1:  # 동 후진
         y -= 1
-        return True
-    return False
-
-
-def print_map():
-    for i in range(n):
-        for j in range(m):
-            print(maps[i][j], end=" ")
-        print()
+        return x, y
+    return n + 1, m + 1
 
 
 while True:
     if maps[r][c] == 0:
         maps[r][c] = 2
         ans += 1
-        print("=====================")
-        print_map()
 
     if check_dust(r, c):  # 먼지 있는 경우
         if d == 0:
@@ -71,8 +63,9 @@ while True:
             d = 1
         elif d == 3:
             d = 2
-        move_rvc(r, c, d)
+        r, c = move_rvc(r, c, d)
     else:
-        if not back_rvc(r, c, d):
+        r, c = back_rvc(r, c, d)
+        if r == n + 1 and c == m + 1:
             break
 print(ans)
