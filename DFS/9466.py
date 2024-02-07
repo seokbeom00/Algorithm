dafea6058cccb7755c48cc91fa639bf4f29df523
HongGit <soojam@konkuk.ac.kr>
2024-02-07 14:43:07 +0900

    9466 solved

--- DFS/9466.py
+++ DFS/9466.py
@@ -1,31 +1,31 @@
 import sys
-from collections import deque
-
-sys.setrecursionlimit(1000000)
 
 input = sys.stdin.readline
+
+sys.setrecursionlimit(111111)
+
 t = int(input())
 
 
-def dfs(idx, tmp):
+def dfs(idx):
+    global ans
     tmp.append(idx)
     visit[idx] = True
-    if s[idx] in tmp:
-        return len(tmp) - tmp.index(s[idx])
-    if not visit[s[idx]]:
-        dfs(s[idx], tmp)
-    return 0
+    if visit[s[idx]]:
+        if s[idx] in tmp:
+            ans -= len(tmp) - tmp.index(s[idx])
+            return
+    else:
+        dfs(s[idx])
 
 
 for i in range(t):
     n = int(input())
     visit = [False] * (n + 1)
-    s = deque(list(map(int, input().split())))
-    s.appendleft(0)
-    ans = 0
+    s = [0] + list(map(int, input().split()))
+    ans = n
     for i in range(1, n + 1):
         if not visit[i]:
             tmp = []
-            ans += dfs(i, tmp)
-            print(tmp)
+            dfs(i)
     print(ans)
