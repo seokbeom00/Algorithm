01dde9d5fb38de9d48e3138144b935afcdd11429
홍석범 <soojam@konkuk.ac.kr>
2025-02-03 15:19:05 +0900

    [Silver III] Title: 타노스, Time: 108 ms, Memory: 14476 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.java
+++ 백준/Silver/20310. 타노스/타노스.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         String s = br.readLine();
         String[] parts = s.split("");
+        boolean[] flag = new boolean[parts.length];
         int one = 0;
         int zero = 0;
         for (String S : parts) {
@@ -15,12 +16,31 @@ public class Main {
                 one += 1;
             }
         }
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < zero / 2; i++) {
-            sb.append("0");
+        one = one / 2;
+        zero = zero / 2;
+        for (int i = 0; i < parts.length; i++) {
+            if (one == 0) {
+                break;
+            }
+            if (parts[i].equals("1")) {
+                flag[i] = true;
+                one -= 1;
+            }
         }
-        for (int i = 0; i < one / 2; i++) {
-            sb.append("1");
+        for (int i = parts.length-1; i >= 0; i--) {
+            if (zero == 0) {
+                break;
+            }
+            if (parts[i].equals("0")) {
+                flag[i] = true;
+                zero -= 1;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < parts.length; i++) {
+            if (!flag[i]) {
+                sb.append(parts[i]);
+            }
         }
         System.out.println(sb);
     }
