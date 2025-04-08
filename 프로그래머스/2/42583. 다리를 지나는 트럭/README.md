7c405dfb7a019e9bbf85aba41d1f65907efd15a7
홍석범 <soojam@konkuk.ac.kr>
2025-04-08 16:22:08 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 58.56 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
@@ -1,10 +1,10 @@
 # [level 2] 다리를 지나는 트럭 - 42583 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583) 
 
 ### 성능 요약
 
-메모리: 98.8 MB, 시간: 23.71 ms
+메모리: 103 MB, 시간: 58.56 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 08일 16:04:26
+2025년 04월 08일 16:22:06
 
 ### 문제 설명
 
--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,26 +1,54 @@
 import java.util.*;
 
 class Solution {
+    class Truck{
+        int weight;
+        int move;
+        public Truck(int weight){
+            this.weight = weight;
+            this.move = 1;
+        }
+        public void moving() {
+            move++;
+        }
+    }
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        Queue<Integer> bridge = new LinkedList<>();
         int time = 0;
-        int sum = 0;
-        int index = 0;
-        while (index < truck_weights.length) {
+        Queue<Truck> waiting = new LinkedList<>();
+        Queue<Truck> moving = new LinkedList<>();
+        
+        for(int i=0; i<truck_weights.length; i++){
+            waiting.offer(new Truck(truck_weights[i]));
+        }
+        
+        int current = 0;
+        while(!waiting.isEmpty() || !moving.isEmpty()){
             time++;
-            if(bridge.size() == bridge_length){
-                sum -= bridge.poll();
+            
+            if(moving.isEmpty()){
+                Truck truck = waiting.poll();
+                current = truck.weight;
+                moving.offer(truck);
+                continue;
             }
-            int truck = truck_weights[index];
-            if(sum + truck > weight){
-                bridge.offer(0);
-            }else{
-                bridge.offer(truck);
-                sum += truck;
-                index++;
+            
+            for (Truck t : moving) {
+                t.moving();
+            }
+            
+            Truck truck = moving.peek();
+            if(truck.move == bridge_length + 1){
+                moving.poll();
+                current -= truck.weight;
+            }
+            
+            if(!waiting.isEmpty() && waiting.peek().weight + current <= weight){
+                Truck t = waiting.poll();
+                moving.offer(t);
+                current += t.weight;
             }
         }
-        time += bridge_length;
+
         return time;
     }
 }
