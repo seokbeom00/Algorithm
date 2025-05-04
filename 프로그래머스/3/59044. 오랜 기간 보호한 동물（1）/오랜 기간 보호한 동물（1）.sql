f9f4ad1c5fe02d5ae60d25fe4992dff2da35383a
홍석범 <soojam@konkuk.ac.kr>
2025-05-04 23:15:44 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,7 +1,6 @@
-select a1.name, a1.datetime
-from animal_ins a1
-where a1.animal_id not in(select a2.animal_id 
-                           from animal_outs a2
-                          )
-order by a1.datetime
-limit 3
+SELECT NAME, DATETIME
+FROM ANIMAL_INS
+WHERE ANIMAL_ID NOT IN(SELECT ANIMAL_ID
+                      FROM ANIMAL_OUTS)
+ORDER BY DATETIME
+LIMIT 3
