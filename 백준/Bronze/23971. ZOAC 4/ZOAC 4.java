fa93bef500f100b72991c42ae9205d329f08bf3c
홍석범 <soojam@konkuk.ac.kr>
2026-04-08 20:35:48 +0900

    [Bronze III] Title: ZOAC 4, Time: 100 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Bronze/23971. ZOAC 4/ZOAC 4.java
+++ 백준/Bronze/23971. ZOAC 4/ZOAC 4.java
@@ -1,24 +1,25 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line = br.readLine();
-        String[] parts = line.split(" ");
-        int h = Integer.parseInt(parts[0]); //열
-        int w = Integer.parseInt(parts[1]); //행
-        int n = Integer.parseInt(parts[2]); //열 간격
-        int m = Integer.parseInt(parts[3]); //행 간격
-
-        int column = w / (m + 1);
-        if (w % (m + 1) != 0) {
-            column += 1;
+        String s = br.readLine();
+        String[] line = s.split(" ");
+        int h = Integer.parseInt(line[0]);
+        int w = Integer.parseInt(line[1]);
+        int n = Integer.parseInt(line[2]);
+        int m = Integer.parseInt(line[3]);
+//        System.out.println(m);
+//        w개씩 h줄 행끼리 N칸 열끼리 m칸
+        int x = w / (m + 1);
+        if (w % (m + 1) > 0) {
+            x++;
         }
-        int row = h / (n + 1);
-        if (h % (n + 1) != 0) {
-            row += 1;
+        int y = h / (n + 1);
+        if (h % (n + 1) > 0) {
+            y++;
         }
-        System.out.println(column * row);
+        System.out.println(x * y);
     }
 }
